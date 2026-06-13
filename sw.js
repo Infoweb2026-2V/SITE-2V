95e5a736bd649474ff1bc58c5a5738145f316154
Infoweb2026-2V <infowebifrn2026@gmail.com>
2026-06-13 15:15:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = 'meu-app-cache-v1';
+const assets = ['./', './index.html', './style.css', './script.js'];
+
+// Instala o Service Worker e guarda os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Executa as requisições mesmo se estiver offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
 
